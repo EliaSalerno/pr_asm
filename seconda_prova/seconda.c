6e51fc3c454edfd5bbcdfc44d796dae08f4732b8
EliaSalerno <salel89.do@gmail.com>
2025-06-23 14:42:08 +0000

    Master: add new test

---
+++ seconda_prova/seconda.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int num1=0, num2=0, num3=0;
+	printf("Inserisci il primo numero: ");
+	scanf("%d",&num1);
+	printf("Inserisci il secondo numero: ");
+	scanf("%d",&num2);
+	int num4=100;
+	printf("Il risultato e': %d",num3);
+	return 0;
+}
