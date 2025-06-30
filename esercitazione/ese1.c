7784928afdbf144df047c2096e5ebf8cbc3f9247
EliaSalerno <salel89.do@gmail.com>
2025-06-30 13:26:51 +0000

    Master: add new directory

---
+++ esercitazione/ese1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int num1=0, num2=0, risultato=0;
+	printf("NUM1= ");
+	scanf("%d",&num1);
+	printf("NUM2= ");
+        scanf("%d",&num2);
+	int trova=100;
+	printf("Il risultato e': %d",risultato);
+	return 0;
+}
