38c1a1be2a37e0c47089956205556d9fbaf53a27
EliaSalerno <salel89.do@gmail.com>
2025-07-07 11:08:35 +0200

    Master: add ese

---
+++ Ese/ese3/ese3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int n1=0,n2=0,somma=0;
+	printf("Inserire n1: ");
+	scanf("%d",n1);
+	printf("Inserire n2: ");
+	scanf("%d",n2);
+	
+	int trovato=100;
+	printf("La somma è: %d",&somma);
+	return 0;
+}
